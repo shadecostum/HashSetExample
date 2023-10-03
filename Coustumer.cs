41f08205068c8dfe646a178382356328adeddae0
Akash santhosh <akash777kk@gmail.com>
2023-10-03 22:19:53 +0530

    Add project files.

---
+++ Coustumer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashSetExample
+{
+    internal class Coustumer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public Coustumer(int id,string name) 
+        {
+            Id = id;
+            Name = name;
+        }
+        public override int GetHashCode()
+        {
+            return Id;
+        }
+        public override bool Equals(object? obj)
+        {
+            Coustumer coustumer = obj as Coustumer;
+            return coustumer.Id==Id;
+        }
+        public override string ToString()
+        {
+            return $"{Id} , {Name}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿namespace HashSetExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<int> list = new HashSet<int>
+            {
+                1,
+                2,
+                3,1
+            };
+
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+
+
+            HashSet<Coustumer> set = new HashSet<Coustumer>
+           {
+               new Coustumer(1,"Akash"),
+               new Coustumer(2,"allen"),
+               new Coustumer(3,"babu"),
+               new Coustumer(2,"king")
+           };
+
+            Console.WriteLine("Count:"+set.Count);
+            foreach (var item in set)
+            {
+                Console.WriteLine(item.Id+","+item.Name);
+            }
+
+            Coustumer coustumer = new Coustumer(4,"aas");
+            Console.WriteLine(coustumer);
+
+            foreach (var item in set)
+            {
+                Console.WriteLine(item.Id+","+item.Name);
+            }
+
+        }
+    }
+}
